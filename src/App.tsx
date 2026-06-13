f63422b3dd4e7a82f6d2fe8bca6273738b31c21d
EerieGoesD <eeriegoesd@gmail.com>
2026-06-13 23:54:37 +0100

    match Made-by font-weight to other footer links

--- src/App.tsx
+++ src/App.tsx
@@ -596,7 +596,7 @@ function DesktopApp({ theme, onToggleTheme }: AppViewProps) {
             <span className="sep">|</span>
             <a className="link-coffee" href="https://buymeacoffee.com/eeriegoesd" target="_blank" rel="noreferrer">Support This Project</a>
             <span className="sep">|</span>
-            <span>Made by</span>
+            <span className="made-by">Made by</span>
             <a className="link-eerie" href="https://eeriegoesd.com/" target="_blank" rel="noreferrer">EERIE</a>
           </div>
         </footer>
